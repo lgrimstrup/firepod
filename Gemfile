bf4b84b6420cb9563d94add266bb9a04bf3a83ce
Luke Grimstrup <luke.grimstrup@citrix.com>
2013-03-07 02:21:33 +1300

    added podio validation for webhooks

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-source :rubygems
+source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'unicorn'
+
+gem 'podio'
 
 group :deploy do
   gem 'capistrano'
--- firepod.rb
+++ firepod.rb
@@ -1,12 +1,34 @@
+require 'config/config'
 require 'sinatra'
+require 'podio'
 
 class Firepod < Sinatra::Base
   configure :production, :development do
     enable :logging
   end
 
+  Podio.setup(
+    :api_key    => PODIO[:api_key],
+    :api_secret => PODIO[:api_secret]
+  )
+
   get '/' do
-    logger.info params.inspect
-    "Hello"
+    "Firepod is running..."
+  end
+
+  post '/' do
+    case params['type']
+      when 'hook.verify'
+        # Validate the webhook
+        Podio::Hook.validate(params['hook_id'], params['code'])
+
+      when 'item.create', 'item.update'
+        Podio.client.authenticate_with_credentials(PODIO[:username], PODIO[:password])
+
+        logger.info params.inspect
+        params.inspect
+      when 'item.delete'
+        # Do something. item_id is available in params['item_id']
+    end
   end
 end
